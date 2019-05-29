8793f3de8708fe8ce6b1f945ecf4b44be4fd4adb
min_2271 <min_2271@naver.com>
2019-05-29 20:34:27 +0900

    Channel Nested API 추가

--- channel/serializers.py
+++ channel/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
 from .models import Channel
+from video.models import Video
 
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
+        fields = '__all__'
+
+class ChanelVideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
         fields = '__all__'
